1c1604b2405d2b5d56a3b7388a3aa6434f720ece
vevhack <vevhack@gmail.com>
2015-04-28 21:28:24 +0800

    介绍了几个回调函数

---
+++ hour04/example05/app.js
@@ -0,0 +1,21 @@
+//sleep实现
+function sleep(milliseconds) {
+    var start = new Date().getTime();
+    while ((new Date().getTime() - start) < milliseconds) {
+    }
+}
+
+function fetchPage() {
+    console.log('fetching page');
+    sleep(2000); // simulate time to query a base
+    console.log('data returned from requesting page');
+}
+
+function fetchApi() {
+    console.log('fetching api');
+    sleep(2000); // simulate time to fetch from an api
+    console.log('data returned from the api');
+}
+
+fetchPage();
+fetchApi();
