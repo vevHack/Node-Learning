225d4d55ded3f8494764fca307d8ebe5a27e2796
vevhack <vevhack@gmail.com>
2015-05-02 10:50:52 +0800

    hour5

--- hour05/example02/server.js
+++ hour05/example02/server.js
@@ -3,9 +3,9 @@ var http = require('http');
 http.createServer(function (req, res) {
     console.log(req);
     res.writeHead(200, {
-        'Content-Type': 'text/plain'
+        'Content-Type': 'text/html'
     });
-    res.end('Hello World\n');
+    res.end('<div><h1>Hello</h1></div>');
 }).listen(3000, "127.0.0.1");
 console.log('Server running at http://127.0.0.1:3000/');
 
