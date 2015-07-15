fed7ad279fc82d0750931a3931563c0dfbc046e2
vevhack <vevhack@gmail.com>
2015-07-15 15:58:51 +0800

    Every Day JavaScript day1

--- DayJs/day3/day3.js
+++ DayJs/day3/day3.js
@@ -1,3 +1,41 @@
 /**
  * Created by vevHack on 2015/7/15.
  */
+for (var i = 1; i <= 3; i++) {
+    setTimeout(function () {
+        console.log(i);
+    }, 0)
+}
+
+var start = new Date();
+setTimeout(function () {
+    var end = new Date();
+    console.log('耗时', end - start, 'ms');
+}, 500);
+while (new Date() - start < 1000) {
+}
+
+var obj = {};
+console.log(obj.foo);
+obj.foo = 'bar';
+
+var fireCount = 0;
+var start = new Date();
+var timer = setInterval(function () {
+    if (new Date() - start > 1000) {
+        clearInterval(timer);
+        console.log(fireCount);
+        return;
+    }
+}, 0);
+//requestAnimationFrame()
+//process.trick()
+var functionHasReturned = false;
+asyncFunction(function () {
+    console.assert(functionHasReturned)
+});
+functionHasReturned = true;
+
+var calculationCache = {},
+    calculationCallbacks = {},
+    mathWorker = new Worker('calculator.js');
