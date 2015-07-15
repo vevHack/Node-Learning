e433b5a547e8909ef873be9d97346f04b04feb33
vevhack <vevhack@gmail.com>
2015-07-15 13:52:18 +0800

    Every Day JavaScript day1

--- DayJs/day2/day2.js
+++ DayJs/day2/day2.js
@@ -1,3 +1,65 @@
 /**
  * Created by vevHack on 2015/7/15.
  */
+var setCommand = function (button, command) {
+    button.onclick = function () {
+        command.execute()
+    }
+};
+var MenuBar = {
+    refresh: function () {
+        console.log('刷新菜单目录');
+    }
+};
+var SubMenu = {
+    add: function () {
+        console.log('增加子菜单');
+    },
+    del: function () {
+        console.log('删除子菜单');
+    }
+};
+var RefreshMenuBarCommand = function (receiver) {
+    this.receiver = receiver;
+};
+RefreshMenuBarCommand.prototype.execute = function () {
+    this.receiver.refresh();
+};
+var AddSubMenuCommand = function (receiver) {
+    this.receiver = receiver;
+};
+AddSubMenuCommand.prototype.execute = function () {
+    this.receiver.add();
+};
+var DelSubMenuCommand = function (receiver) {
+    this.receiver = receiver;
+};
+DelSubMenuCommand.prototype.execute = function () {
+    this.receiver.del();
+};
+
+var refreshMenuBarCommand = new RefreshMenuBarCommand(MenuBar);
+var addSubMenuCommand = new AddSubMenuCommand(SubMenu);
+var delSubMenuCommand = new DelSubMenuCommand(SubMenu);
+
+setCommand(button1, refreshMenuBarCommand);
+setCommand(button2, addSubMenuCommand);
+setCommand(button3, delSubMenuCommand);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
